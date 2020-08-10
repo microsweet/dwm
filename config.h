fb003fe9697d0063a3bbf15e927e82507c2f68e6
microsweet <microsweet@email.com>
2020-08-10 08:32:11 +0800

    set eclipse to mask 3

--- config.h
+++ config.h
@@ -59,6 +59,7 @@ static const Rule rules[] = {
 	{ "quemu-system-i386", 	NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  		NULL,       NULL,       1 << 8,       0,           -1 },
 	{ "Google-chrome",  	NULL,       NULL,       2,            0,            0},
+	{ "Eclipse",  		NULL,       NULL,       1 << 2,       0,            0},
 };
 
 /* layout(s) */

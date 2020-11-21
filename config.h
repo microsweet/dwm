c14d579dbb109b77591f5a9924b281220a11746e
Microsweet <weide.yang@hotmail.com>
2020-11-21 11:39:54 +0800

    bar bottom

--- config.h
+++ config.h
@@ -15,7 +15,7 @@ static const unsigned int gappoh    = 3;       /* horiz outer gap between window
 static const unsigned int gappov    = 3;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
+static const int topbar             = 0;        /* 0 means bottom bar */
 static const Bool viewontag         = True;     /* Switch view on tag switch */
 static const char *fonts[]          = { "SauceCodePro Nerd Font Mono:size=14" };
 static const char dmenufont[]       = "SauceCodePro Nerd Font Mono:size=16";
@@ -60,6 +60,7 @@ static const Rule rules[] = {
 	{ "firefox",  		NULL,       NULL,       1 << 1,       0,           -1 },
 	{ "Google-chrome",  	NULL,       NULL,       2,            0,            0},
 	{ "Eclipse",  		NULL,       NULL,       1 << 2,       0,            0},
+	{ "thunder",  		NULL,       NULL,       0,            1,            0},
 };
 
 /* layout(s) */
